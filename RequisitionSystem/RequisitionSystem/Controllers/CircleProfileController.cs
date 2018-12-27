89b2e1d98c7bd1f673238690157753446d749819
joyspl <joy@hotmail.co.in>
2018-12-27 14:41:12 +0530

    circle profile & other user profile show except district

---
+++ RequisitionSystem/RequisitionSystem/Controllers/CircleProfileController.cs
@@ -0,0 +1,23 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RequisitionSystem.Controllers
+{
+    public class CircleProfileController : Controller
+    {
+        //
+        // GET: /CircleProfile/
+
+        public ActionResult Index()
+        {
+            var objCircleProfile = DBOperations<CircleProfile>.GetSpecific(new CircleProfile() { Opmode = 0, CircleMasterId=GlobalSettings.oUserMaster.RefId }, Constant.usp_CircleProfile);
+
+            return View(objCircleProfile);
+        }
+
+    }
+}
---
+++ RequisitionSystem/RequisitionSystem/Models/CircleProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+    public class CircleProfile
+    {
+        public long CircleMasterId { get; set; }
+        public string CircleCode { get; set; }
+        public string CircleName { get; set; }
+        public long DistrictId { get; set; }
+        public string CircleOfficerName { get; set; }
+        public string CircleAddress { get; set; }
+        public string CirclePin { get; set; }
+        public string AlterMobile { get; set; }
+        public string PoliceStation { get; set; }
+        public int Opmode { get; set; }
+    }
